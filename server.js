64c1ead2afff06dfde0bc679dca0417c281879b8
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-03-09 16:07:45 +0530

    readme file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require("dotenv").config()
-const PORT = process.env.PORT;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to ForgeSavant API!');
