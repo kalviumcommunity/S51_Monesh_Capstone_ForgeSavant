a3ce10f79dd4a893c1ab4e15d2c866478015bd0e
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-03-12 16:48:46 +0530

    FrontEnd

---
+++ client/frontEnd/src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css'
+
+const About = () => {
+  return (
+    <div className="About">
+      <h1>ForgeSavant</h1>
+      <p>ForgeSavant is a web application aimed at providing users with a seamless experience in checking and building custom PC configurations virtually. It offers a comprehensive platform to assess compatibility, performance scores, and real-time prices of selected components, empowering users to make informed decisions when crafting their ideal PC builds.</p>
+      <a href="https://wiki.kalvium.community/doc/monesh-NGLggHlAzk" target="_blank" rel="noopener noreferrer">Project Details</a>
+    </div>
+  );
+};
+
+export default About;
