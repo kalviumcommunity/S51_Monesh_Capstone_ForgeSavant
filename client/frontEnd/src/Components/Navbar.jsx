cf7ad93d553712273075e4b3265606509c477b18
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-03-23 16:18:28 +0530

    UI updated

--- client/frontEnd/src/Components/Navbar.jsx
+++ client/frontEnd/src/Components/Navbar.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 import '../Styles/navbar.css';
 import logo from "../assets/ForgeSavant1.png";
+import menu from "../assets/Menu.png";
 
 const NavBar = () => {
+  const [showSideNav, setShowSideNav] = useState(false);
+
+  const animation = useSpring({
+    from: { opacity: 0, transform: 'translateY(30px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    config: { duration: 1000 },
+  });
+
+  const toggleSideNav = () => {
+    setShowSideNav(!showSideNav);
+  };
+
   return (
-    <nav className="Navbar" aria-label="Main Navigation">
-      <div id="left-nav">
-        <img src={logo} alt="Forge Savant Logo" id="logo" />
-      </div>
-      <div id="right-nav">
-        <h3 className="nav-item">HOME</h3>
-        <h3 className="nav-item">ABOUT</h3>
-        <button className="nav-button">LOGIN</button>
-      </div>
-    </nav>
+    <animated.div style={animation} className="navbar"> 
+        <div className="navbar-left">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="navbar-right">
+          <p>HOME</p>
+          <img
+            src={menu}
+            alt="Menu"
+            className="menu-icon"
+            onClick={toggleSideNav}
+          />
+        </div>
+      {showSideNav && (
+        <div className="side-navbar">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">COMP</a>
+        </div>
+      )}
+    </animated.div>
   );
-}
+};
 
 export default NavBar;
