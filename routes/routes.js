807ab3e8b11ee6de960a78b50eb5202ad411a269
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-06 12:03:21 +0530

    Created Routes for all components

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const Processor = require("../models/processor.model");
 const GraphicsCard = require("../models/graphicsCard.model")
 const Motherboard = require("../models/motherboard.model")
-const Ram = require("../models/ram.model")
+const RAM = require("../models/ram.model")
 const Storage = require("../models/storage.model")
-const smps = require("../models/smps.model")
-const cabinets = require("../models/cabinet.model")
+const SMPS = require("../models/smps.model")
+const Cabinets = require("../models/cabinet.model")
 
 router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,6 +14,8 @@ router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+//GPU
 
 router.get("/GPU/:id", async (req, res) => {
   try {
@@ -27,7 +29,7 @@ router.get("/GPU/:id", async (req, res) => {
     res.json(graphicCard);
   } catch (err) {
     console.log("Error while fetching the GPU:", err);
-    res.status(500).json({ message: "Error occurred while fetching the Graphics Card" });
+    res.status(500).json({ message: "There was an error processing your request." });
   }
 });
 
@@ -37,9 +39,11 @@ router.get("/GPU", async (req, res) => {
     res.json(graphicCard);
   } catch (err) {
     console.log("Error occurred while fetching GPU data:", err);
-    res.status(500).json({ message: "Error occurred while fetching GPU data" });
+    res.status(500).json({ message: "There was an error processing your request." });
   }
 });
+
+//CPU
 
 router.get("/CPU/:id", async (req, res) => {
   try {
@@ -53,7 +57,7 @@ router.get("/CPU/:id", async (req, res) => {
     res.json(processor);
   } catch (err) {
     console.error("Error fetching the processor:", err);
-    res.status(500).json({ message: "Error occurred while fetching the processor" });
+    res.status(500).json({ message: "There was an error processing your request." });
   }
 });
 
@@ -63,8 +67,146 @@ router.get("/CPU", async (req, res) => {
     res.json(processor);
   } catch (err) {
     console.error("Error occurred while fetching CPU data:", err);
-    res.status(500).json({ message: "Error occurred while fetching CPU data" });
+    res.status(500).json({ message: "There was an error processing your request." });
   }
 });
+
+// Cabinets
+
+router.get("/cabinet/:id", async (req, res) => {
+  try{
+    const cabinetId = req.params.id;
+    const cabinet = await Cabinets.findById(cabinetId)
+
+    if (!cabinet){
+      return res.status(404).json({message:"Cabinet Not Found!"})
+    }
+
+    res.json(cabinet);
+  } catch (err) {
+    console.log("Error fetching the cabinet: ", err);
+    res.status(500).json({ message: "There was an error processing your request"})
+  }
+})
+
+router.get("/cabinet", async (req, res) => {
+  try{
+    const cabinet = await Cabinets.find();
+    res.json(cabinet);
+  } catch (err) {
+    console.error("Error occurred while getting cabinet info:", err);
+    res.status(500).json({ message: "There was an error processing your request." })
+  }
+})
+
+// Storage
+
+router.get("/storage/:id", async (req, res) => {
+  console.log(Storage.findById(req.params.id));
+  try {
+    const storage = await Storage.findById(req.params.id);
+    
+
+    if (!storage) {
+      return res.status(404).json( { message: 'Storage not found.'} );
+    }
+  
+    res.json(storage);
+  } catch (err) {
+    console.log("Error occured while getting the storage device details", err)
+    res.status(500).json({ message: "Server Error" });
+  }
+})
+
+router.get("/storage", async (req, res) => {
+  try{
+    const storage = await Storage.find();
+    res.json(storage);
+  } catch (err) {
+    console.error("Error occurred in retrieving storage device data.", err);
+    res.status(500).json({message:"There was an error processing your request."})
+  }
+})
+
+// SMPS
+
+router.get("/smps/:id", async (req, res) => {
+  try{
+    const smps = await SMPS.findById(req.params.id);
+
+    if (!smps){
+      return res.status(404).json({ message: "SMPS device data not found"});
+    }
+
+    res.json(smps);
+  } catch (err) {
+    console.error("Error occured in retrieving storage device data.", err);
+    res.status(500).json({ message: "There was an error processing your request." })
+  }
+})
+
+router.get("/smps", async (req, res) => {
+  try{
+    const smps = await SMPS.find();
+    res.json(smps);
+  } catch (err) {
+    console.error("Error occured in retrieving SMPS device data")
+    res.status(500).json({ message:"There was an error processing your request"})
+  }
+})
+
+// Motherboard
+
+router.get("/motherboard/:id", async (req, res) => {
+  try{
+    const motherboard = await Motherboard.findById(req.params.id);
+
+    if (!motherboard) {
+      return res.status(404).json({ message: "Motherboard data not found." });
+    }
+
+    res.json(motherboard);
+  } catch (err) {
+    console.error("Error occurred while getting motherboard data.", err);
+    res.status(500).json({ message: "There was an error processing your request"})
+  }
+})
+
+router.get("/motherboard", async (req, res) => {
+  try{
+    const motherboard = await Motherboard.find();
+    res.json(motherboard);
+  } catch (err) {
+    console.error("Error occured in retrieving MotherBoard devices data")
+    res.status(500).json({ message: "There was an error processing your request"})
+  }
+})
+
+// RAM
+
+router.get("/ram/:id", async (req, res) => {
+  try{
+    const ram = await RAM.findById(req.params.id);
+    
+    if (!ram){
+      return res.status(404).json({ message: "RAM data was not found"})
+    }
+
+    res.json(ram);
+  } catch (err) {
+    console.error("Error occured in retrieving RAM device data");
+    res.status(500).json({message: "There was an error processing your request"});
+  }
+})
+
+router.get("/ram", async (req, res) => {
+  try{
+    const ram = await RAM.find();
+    res.json(ram);
+  } catch (err) {
+    console.error("Error occured in retrieving RAM device data");
+    res.status(500).json({ message: "There was an erro processing your request"})
+  }
+})
 
 module.exports = router;
