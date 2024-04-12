7e5bfce03a6e56612cbea0f23146f155f5e8fbd5
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-12 11:07:07 +0530

    Added Navbar for better navigation

--- client/frontEnd/src/Components/Navbar.jsx
+++ client/frontEnd/src/Components/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Styles/Navbar.css'
+import '../Styles/Navbar.css';
 import brandLogo from '../assets/ForgeSavant2.png';
-
 
 const Navbar = () => {
   return (
     <>
-        <div className='navbar'>
-            <img src={brandLogo} alt="" />
-            <Link to="/build"><button id='build'>Forge your PC</button></Link>
-        </div> 
+      <div className='navbar'>
+        <img src={brandLogo} alt="Forge Savant Logo" />
+        <Link to="/build"><button id='build'>Forge your PC</button></Link>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
