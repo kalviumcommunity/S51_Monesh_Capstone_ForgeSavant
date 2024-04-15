4789fb8ce2655aa9bc0bf970accf3cb037cae455
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-15 11:56:42 +0530

    Added google oauth

--- client/frontEnd/src/main.jsx
+++ client/frontEnd/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="208063553262-toavnmm8l19j85fhsc4he8e993v22n0k.apps.googleusercontent.com">;
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
