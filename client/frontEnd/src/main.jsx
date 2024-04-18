4ba2e09815e29cf68deb02cc8c397035cc0e3fcb
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-18 14:50:08 +0530

    Compatibility Logic

--- client/frontEnd/src/main.jsx
+++ client/frontEnd/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="208063553262-toavnmm8l19j85fhsc4he8e993v22n0k.apps.googleusercontent.com">;
+    <GoogleOAuthProvider clientId="208063553262-toavnmm8l19j85fhsc4he8e993v22n0k.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
