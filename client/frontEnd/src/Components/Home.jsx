1e1e435d84521a4172937b7625296bf167ba709e
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-24 16:57:03 +0530

    Changed Home UI

--- client/frontEnd/src/Components/Home.jsx
+++ client/frontEnd/src/Components/Home.jsx
@@ -6,26 +6,6 @@ import "../Styles/home.css";
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const pageElements = document.querySelectorAll(".section2");
-
-      pageElements.forEach((element) => {
-        const elementOffset = element.offsetTop;
-        if (currentScrollY > elementOffset - windowHeight / 2) {
-          setIsVisible(true);
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   const slideIn = useSpring({
     from: { transform: "translateX(-20%)", opacity: 0 },
     to: { transform: "translateX(0)", opacity: 1 },
@@ -39,107 +19,21 @@ const Home = () => {
   });
 
   return (
-    <div style={{ overflow: "hidden" }}>
-      <div className="page1">
-        <animated.div style={slideIn}>
-          <h1>Forge</h1>
-          <h1>Savant</h1>
-        </animated.div>
+    <>
+      <div className="Display">
+        <div className="MainBody">
+          <div className="marquee">
+            <span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span>
+            <span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span>
+          </div>
+          <div className="BrandName"><h1>Forge Savant</h1></div>
+          <div className="marquee">
+            <span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span>
+            <span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span><span>| Explicit Content</span>
+          </div>
+        </div>
       </div>
-
-      <div className="page2 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Welcome to Forge Savant!</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Forge Savant is your go-to destination for crafting the perfect PC
-          tailored to your desires. Whether you're a gaming enthusiast, a
-          creative professional, or a tech-savvy user, Forge Savant empowers
-          you to build your dream rig effortlessly.
-        </animated.p>
-      </div>
-
-      <div className="page3 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Why Choose Forge Savant?</animated.h1>
-      </div>
-
-      <div className="page4 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Seamless Component Selection</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Choose from a curated selection of top-tier processors, graphics
-          cards, motherboards, RAM, storage, power supplies, and cabinets.
-        </animated.p>
-      </div>
-
-      <div className="page5 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Intelligent Compatibility Checks</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Our cutting-edge compatibility checker ensures that every component
-          you select works harmoniously together, eliminating any guesswork.
-        </animated.p>
-      </div>
-
-      <div className="page6 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Real-Time Performance Insights</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Dive into real-time benchmark scores and performance metrics for
-          each component, empowering you to make informed decisions.
-        </animated.p>
-      </div>
-
-      <div className="page7 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Intuitive User Interface</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Our user-friendly interface guides you through the PC building
-          process, whether you're a novice or an experienced builder.
-        </animated.p>
-      </div>
-
-      <div className="page8 section2 shared-background">
-        <animated.h1 style={isVisible ? appearIn : {}}>Compare Your Options</animated.h1>
-        <animated.p style={isVisible ? appearIn : {}}>
-          Compare and contrast multiple components side by side, finding the
-          perfect balance between performance, aesthetics, and budget.
-        </animated.p>
-      </div>
-
-      <div className="page9 section3 shared-background">
-        <h1>How Forge Savant Works?</h1>
-      </div>
-
-      <div className="page10 section3 shared-background">
-        <h1>Select Your Components</h1>
-        <p>Explore our diverse catalog and choose your preferred processor, 
-          graphics card, motherboard, RAM, storage, power supply, and 
-          cabinet.</p>
-      </div>
-
-      <div className="page11 section3 shared-background">
-        <h1>Verify Compatibility</h1>
-        <p>Our advanced compatibility checker ensures that all your selected 
-          components fit seamlessly together, saving you time and 
-          frustration.</p>
-      </div>
-
-      <div className="page12 section3 shared-background">
-        <h1>Analyze Performance</h1>
-        <p>Dive into detailed benchmarks and performance insights, allowing 
-          you to fine-tune your build for optimal results.</p>
-      </div>
-
-      <div className="page13 section3 shared-background">
-        <h1>Build and Save Configurations</h1>
-        <p>Once satisfied, create your virtual build, save your configurations, 
-          and share them with friends or save for future projects.</p>
-      </div>
-
-      <div className="page14 section3 shared-background">
-        <h1>Start Crafting Your PC Masterpiece Today!</h1>
-        <p>Ready to unleash your creativity and build a PC that matches your 
-          vision? Embark on your journey with Forge Savant today and 
-          experience the satisfaction of crafting your personalized 
-          powerhouse!</p>
-          <Link to="/build"><button id='build'>Forge your PC</button></Link>
-      </div>
-    </div>
+    </>
   );
 };
 
