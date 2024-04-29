9f815d659f4ba58ce60fb2a7256f32200b885e14
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-04-29 18:09:52 +0530

    Simple UI

--- client/frontEnd/src/Components/Build.jsx
+++ client/frontEnd/src/Components/Build.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import '../Styles/Build.css'
+import pc from "../assets/custom-gaming-pc.png"
 
 const Build = () => {
     
     return(
         <React.Fragment>
-            <div className="Build_Page">    
-                    <div className="Build-Title">
-                        <div className="building">
-                            <div className="Selection">
-                                <h3>Choose a Platform</h3>
-                            </div>
-                            <div className="pc">
-
-                            </div>
-                        </div>
-                    </div>
+            <div className="Build_Page">
+                <div className="Build-Title">
+                    <h1><span id="gold">Forge</span> Arena</h1>
+                    <h3>Start Forging your Dream PC</h3>
+                </div>
+                <div className="Build-Area">
+                    <div className="selection">Choose Platform</div>
+                    <div className="pc-img"><img src={pc} alt="" /></div>
+                </div>
             </div>
         </React.Fragment>
     )
