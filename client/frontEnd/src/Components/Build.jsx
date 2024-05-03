27db609da1449a414c359b32a140d603e898b04f
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-05-03 10:01:18 +0530

    Fetching Data

--- client/frontEnd/src/Components/Build.jsx
+++ client/frontEnd/src/Components/Build.jsx
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
-import '../Styles/Build.css'
-import pc from "../assets/custom-gaming-pc.png"
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import '../Styles/Build.css';
+import pc from "../assets/custom-gaming-pc.png";
 
 const Build = () => {
-    
+    const [gpuData, setGpuData] = useState([]);
+    const [cpuData, setCpuData] = useState([]);
+    const [cabinetData, setCabinetData] = useState([]);
+    const [storageData, setStorageData] = useState([]);
+    const [smpsData, setSmpsData] = useState([]);
+    const [motherboardData, setMotherboardData] = useState([]);
+    const [ramData, setRamData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const gpuResponse = await axios.get('http://localhost:5000/GPU');
+                setGpuData(gpuResponse.data);
+
+                const cpuResponse = await axios.get('http://localhost:5000/CPU');
+                setCpuData(cpuResponse.data);
+
+                const cabinetResponse = await axios.get('http://localhost:5000/cabinet');
+                setCabinetData(cabinetResponse.data);
+
+                const storageResponse = await axios.get('http://localhost:5000/storage');
+                setStorageData(storageResponse.data);
+
+                const smpsResponse = await axios.get('http://localhost:5000/smps');
+                setSmpsData(smpsResponse.data);
+
+                const motherboardResponse = await axios.get('http://localhost:5000/motherboard');
+                setMotherboardData(motherboardResponse.data);
+
+                const ramResponse = await axios.get('http://localhost:5000/ram');
+                setRamData(ramResponse.data);
+                console.log(cabinetData)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return(
         <React.Fragment>
             <div className="Build_Page">
@@ -17,7 +57,7 @@ const Build = () => {
                 </div>
             </div>
         </React.Fragment>
-    )
+    );
 };
 
 export default Build;
